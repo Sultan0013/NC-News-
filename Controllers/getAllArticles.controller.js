5da895ab17c66410ea6be8737b49bc4ca251d06b
Sultan0013 <ozairyousufi1400@gmail.com>
2024-07-16 16:50:47 +0100

    fixed typo

--- Controllers/getAllArticles.controller.js
+++ Controllers/getAllArticles.controller.js
@@ -2,8 +2,6 @@ const {fetchAllArticles} = require("../Models/getAllArticles.model")
 const getAllArticles =(req, resp, next)=>{
 
    fetchAllArticles().then((articles)=>{
-    console.log(articles)
-
     resp.status(200).send({articles})
    
 }).catch(error=>{

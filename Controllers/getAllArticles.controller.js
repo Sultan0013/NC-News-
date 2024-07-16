839545cdc536585a11accc377d7e78b7cea97918
Sultan0013 <ozairyousufi1400@gmail.com>
2024-07-16 16:50:16 +0100

    fixed wrong refernece

--- Controllers/getAllArticles.controller.js
+++ Controllers/getAllArticles.controller.js
@@ -2,12 +2,12 @@ const {fetchAllArticles} = require("../Models/getAllArticles.model")
 const getAllArticles =(req, resp, next)=>{
 
    fetchAllArticles().then((articles)=>{
+    console.log(articles)
 
     resp.status(200).send({articles})
    
 }).catch(error=>{
-
-    next(err)
+    next(error)
    })
     
 }

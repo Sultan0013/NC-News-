f9a1d9c7e2d02a270b15e18398a376f979c042b4
Sultan0013 <ozairyousufi1400@gmail.com>
2024-07-18 10:06:15 +0100

    POST/api/articles/:article_id/comments creates a new comment on a specifc article id

--- Models/selectCommentsByID.model.js
+++ Models/selectCommentsByID.model.js
@@ -11,7 +11,6 @@ function selectCommentsByArticle_ID(article_id){
     }
     return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
     .then(({ rows }) => {
-        console.log(rows);
         if (rows.length === 0) {
             return Promise.reject({
                 status: 404,

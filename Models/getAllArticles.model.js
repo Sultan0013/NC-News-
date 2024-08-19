a1ace3d8154134ab3cb82a05ddf2675c7aea88fb
Sultan0013 <ozairyousufi1400@gmail.com>
2024-08-19 22:10:47 +0100

    new comment

--- Models/getAllArticles.model.js
+++ Models/getAllArticles.model.js
@@ -1,20 +1,26 @@
-const db = require('../db/connection')
+const db = require("../db/connection");
 
-
-function fetchAllArticles (sort_by = "created_at" , order='desc' , topic){
-
-  const validColumns = ['author', 'title', 'article_id', 'topic', 'created_at', 'votes', 'article_img_url', 'comment_count'];
-  const validOrders = ['asc', 'desc'];
+function fetchAllArticles(sort_by = "created_at", order = "desc", topic) {
+  const validColumns = [
+    "author",
+    "title",
+    "article_id",
+    "topic",
+    "created_at",
+    "votes",
+    "article_img_url",
+    "comment_count",
+  ];
+  const validOrders = ["asc", "desc"];
   if (!validColumns.includes(sort_by)) {
-      return Promise.reject({ status: 400, msg: 'Invalid sort_by column' });
+    return Promise.reject({ status: 400, msg: "Invalid sort_by column" });
   }
 
   if (!validOrders.includes(order)) {
-      return Promise.reject({ status: 400, msg: 'Invalid order value' });
+    return Promise.reject({ status: 400, msg: "Invalid order value" });
   }
 
-
-  let queryString =  `SELECT 
+  let queryString = `SELECT 
       articles.author, 
       articles.title, 
       articles.article_id, 
@@ -28,47 +34,37 @@ function fetchAllArticles (sort_by = "created_at" , order='desc' , topic){
     LEFT JOIN 
       comments ON articles.article_id = comments.article_id`;
 
-    
-    
-  
-      if(topic){
-        queryString += 
-       ` WHERE articles.topic = $1
+  if (topic) {
+    queryString += ` WHERE articles.topic = $1
           GROUP BY 
           articles.article_id
         ORDER BY 
-         ${sort_by} ${order};`
-         
-       return db.query(queryString , [topic] ).then(({rows})=>{
-    
-        if(rows.length === 0){
-          return Promise.reject({
-            status: 404,
-            msg : "Not Found"
-          })
-        }
-          return rows
-         })
-      }
+         ${sort_by} ${order};`;
 
-      queryString += 
-      ` GROUP BY 
+    return db.query(queryString, [topic]).then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "Not Found",
+        });
+      }
+      return rows;
+    });
+  }
+
+  queryString += ` GROUP BY 
          articles.article_id
        ORDER BY 
-        ${sort_by} ${order};`
-         return db.query(queryString ).then(({rows})=>{
-          if(rows.length === 0){
-
-          
-          return Promise.reject({
-            status: 404,
-            msg : "Not Found"
-          })}
-          return rows
-         })
-  
+        ${sort_by} ${order};`;
+  return db.query(queryString).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: "Not Found",
+      });
+    }
+    return rows;
+  });
 }
 
-
-
-module.exports = {fetchAllArticles}
+module.exports = { fetchAllArticles };
--- app.js
+++ app.js
@@ -1,36 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const userRouter = require('./routes/users');
-const articleRouter = require('./routes/article');
-const topicRouter = require('./routes/topics');
-const commentRouter = require('./routes/comments')
-const getAllapi = require('./Controllers/getAPI.controller.js')
+const cors = require("cors");
+const userRouter = require("./routes/users");
+const articleRouter = require("./routes/article");
+const topicRouter = require("./routes/topics");
+const commentRouter = require("./routes/comments");
+const getAllapi = require("./Controllers/getAPI.controller.js");
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", userRouter);
+app.use("/api/articles", articleRouter);
+app.use("/api/topics", topicRouter);
+app.use("/api/comments", commentRouter);
 
-app.use('/api/users', userRouter);
-app.use('/api/articles', articleRouter);
-app.use('/api/topics', topicRouter);
-app.use('/api/comments' , commentRouter)
-
-
-app.get('/api' ,getAllapi )
-
+app.get("/api", getAllapi);
 
 app.use((err, request, response, next) => {
-    if(err.code === '22P02' || err.code === '23502'){
-        response.status(400).send({ msg: "Bad Request" })
-        
-    }else if(err.code === '23503'){
-        response.status(404).send('Not Found' )
-    }
-    
-    else{
-        next(err);
-    }
+  if (err.code === "22P02" || err.code === "23502") {
+    response.status(400).send({ msg: "Bad Request" });
+  } else if (err.code === "23503") {
+    response.status(404).send("Not Found");
+  } else {
+    next(err);
+  }
 });
 app.use((req, res, next) => {
   res.status(404).send({ msg: "Route Not Found" });
@@ -40,6 +34,10 @@ app.use((error, req, res, next) => {
   if (error.status) {
     res.status(error.status).send(error.msg);
   }
-  res.status(500).send({ msg: "Internal Srver Error" });
+  res.status(500).send({ msg: "Internal Srver errro", error });
 });
+
+// app.listen(9000, () => {
+//   console.log("listeingi");
+// });
 module.exports = app;
